0ddd82741edd9e855f243e5bd1cdd6f6905ae6c2
0918029049asd <thinhhuynh@homatechs.com>
2023-03-23 15:59:26 +0700

    add onclick

--- src/component/content_conponent.jsx
+++ src/component/content_conponent.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import request from "../utils/request";
 import VideoItem from "./video_item_component";
 import VideoShow from "./video_show_component";
 function Content() {
     const [videoshow, setvideoshow] = useState('');
+    const [videodesc, setvideodesc] = useState('');
     const [videolist, setvideolist] = useState([]);
     function callAPIData() {
         request.get('videos')
             .then(function (res) {
                 setvideoshow(res.data.items[0].id);
+                setvideodesc(res.data.items[0].snippet.localized.description);
                 setvideolist(res.data.items);
             })
             .catch(function (error) {
@@ -20,14 +22,16 @@ function Content() {
     }, []);
     return (
         <div className="w-full h-90per flex flex-row">
-            <VideoShow comment="hello" id={videoshow}></VideoShow>
-            {
-                console.log(videolist)
-            }
-            <div className="flex flex-col overflow-y-auto">
+            <VideoShow comment={videodesc} id={videoshow}></VideoShow>
+            <div className="flex flex-col overflow-y-auto overflow-x-hidden">
                 {
                     videolist.map(x =>
-                        <VideoItem url={x.snippet.thumbnails.high.url} title={x.snippet.localized.title} info={x.snippet.publishedAt}></VideoItem>
+                        <div onClick={() => {
+                            setvideoshow(x.id);
+                            setvideodesc(x.snippet.localized.description);
+                        }}>
+                            <VideoItem url={x.snippet.thumbnails.high.url} title={x.snippet.localized.title} info={x.snippet.publishedAt}></VideoItem>
+                        </div>
                     )
                 }
             </div>
--- src/component/video_show_component.jsx
+++ src/component/video_show_component.jsx
@@ -4,7 +4,7 @@ function VideoShow(props) {
         <div className="left w-3/4 flex flex-col flex-auto">
             <iframe className="w-4/5 h-2/3 bg-black mx-auto mt-2" src={`http://www.youtube.com/embed/${props.id}`}></iframe>
             <div className="w-11/12 h-1 my-3 mx-auto flex-auto mt-3 bg-orange-400">
-                <p className="m-2 text-white font-sans">{props.comment}</p>
+                <p className="m-2 text-white font-sans w-auto h-44 overflow-hidden">{props.comment}</p>
             </div>
         </div>
     )
